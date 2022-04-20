d4ef7ddba0620c2b406194560994d14ac69a26c9
JonathanNoH <jonathan897h@gmail.com>
2022-04-20 14:58:13 -0600

    change to class

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import General from "./components/General";
 import Education from "./components/Education";
 import Experience from "./components/Experience";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <General />
-      <Education />
-      <Experience />
-    </div>
-  );
+class App extends React.Component {
+  
+  render() {
+    return (
+      <div className="App">
+        <General />
+        <Education />
+        <Experience />
+      </div>
+    );
+  }
 }
 
 export default App;
