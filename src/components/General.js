37d2676d604aa61ee57ec51ba5a5bb9e51184691
JonathanNoH <jonathan897h@gmail.com>
2022-04-20 13:18:13 -0600

    adjust input general

--- src/components/General.js
+++ src/components/General.js
@@ -1,21 +1,62 @@
 import React, { Component } from "react";
 
 class General extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      personName: '',
+      email: '',
+      phone: '',
+    }
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleInputChange = (e) => {
+    const target = e.target;
+    const value = e.value;
+    const name = target.name; 
+
+    this.setState({
+      [name] : value
+    });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+  }
   
   render() {
     return (
-      <form className="general" action="">
+      <form className="general" onSubmit={this.handleSubmit}>
         <div>
-          <label htmlFor="name">Name: </label>
-          <input type="text" name="name" id="name" />
+          <label htmlFor="personName">Name: </label>
+          <input 
+          type="text" 
+          name="personName" 
+          id="personName"
+          value={this.state.personName}
+          onChange={this.handleInputChange} />
         </div>
         <div>
           <label htmlFor="email">Email: </label>
-          <input type="email" name="email" id="email" />
+          <input 
+          type="email" 
+          name="email" 
+          id="email"
+          value={this.state.email}
+          onChange={this.handleInputChange} />
         </div>
         <div>
           <label htmlFor="phone">Phone: </label>
-          <input type="tel" name="phone" id="phone" />
+          <input 
+          type="tel" 
+          name="phone" 
+          id="phone"
+          value={this.state.phone}
+          onChange={this.handleInputChange} />
         </div>
         <input type="submit" value="Update" />
       </form>
